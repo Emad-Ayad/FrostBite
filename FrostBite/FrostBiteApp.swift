446358760d49a6eaa5e7914179aa08b22e58096c
Emad-Ayad <emadayad228@gmail.com>
2026-06-02 14:11:58 +0300

    inital commit

---
+++ FrostBite/FrostBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrostBiteApp.swift
+//  FrostBite
+//
+//  Created by TaqieAllah on 31/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct FrostBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
